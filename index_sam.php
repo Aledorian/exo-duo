15a1867b87c9fa8d513499e526c3f775757aba8d
SamuelMeline <melsam0905@gmail.com>
2022-09-13 16:51:11 +0200

    second commit sam

--- index_sam.php
+++ index_sam.php
@@ -8,7 +8,6 @@
 <body>
     <h1>My Page</h1>
     <p>My Page Content</p>
-<<<<<<< HEAD
     <form action="" method="get" class="form-example">
         <div class="form-example">
             <label for="name">Enter your name: </label>
@@ -25,13 +24,14 @@
 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique vitae accusamus sint provident eius asperiores accusantium officia ducimus nesciunt rem, facilis, doloribus atque vel voluptatibus dolores. Consequatur, tempore iste?</p>
     <p>Reiciendis similique sunt tempore libero ad assumenda veritatis nobis? Vitae sint deleniti perspiciatis reprehenderit aspernatur porro vel magni. Quae saepe in reiciendis corporis libero odio necessitatibus omnis aspernatur eum pariatur!
-    
+
     </p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique vitae accusamus sint provident eius asperiores accusantium officia ducimus nesciunt rem, facilis, doloribus atque vel voluptatibus dolores. Consequatur, tempore iste?
-    
+
     </p>
     <p>Reiciendis similique sunt tempore libero ad assumenda veritatis nobis? Vitae sint deleniti perspiciatis reprehenderit aspernatur porro vel magni. Quae saepe in reiciendis corporis libero odio necessitatibus omnis aspernatur eum pariatur!
     </p>
-    
+
 </body>
+
 </html>

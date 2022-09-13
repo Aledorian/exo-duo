dba755b7af765e50f55a37b1fce58ada3fbcf191
Aledorian <aledorianlegrand@gmail.com>
2022-09-13 16:45:04 +0200

    add function + index modified

--- index_baptiste.php
+++ index_baptiste.php
@@ -6,5 +6,16 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique vitae accusamus sint provident eius asperiores accusantium officia ducimus nesciunt rem, facilis, doloribus atque vel voluptatibus dolores. Consequatur, tempore iste?</p>
+    <p>Reiciendis similique sunt tempore libero ad assumenda veritatis nobis? Vitae sint deleniti perspiciatis reprehenderit aspernatur porro vel magni. Quae saepe in reiciendis corporis libero odio necessitatibus omnis aspernatur eum pariatur!
+    
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor similique vitae accusamus sint provident eius asperiores accusantium officia ducimus nesciunt rem, facilis, doloribus atque vel voluptatibus dolores. Consequatur, tempore iste?
+    
+    </p>
+    <p>Reiciendis similique sunt tempore libero ad assumenda veritatis nobis? Vitae sint deleniti perspiciatis reprehenderit aspernatur porro vel magni. Quae saepe in reiciendis corporis libero odio necessitatibus omnis aspernatur eum pariatur!
+    </p>
 </body>
+
+
 </html>
